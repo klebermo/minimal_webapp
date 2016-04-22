317c9af8056a9db52b8d3890d29be11b483382fd
Kleber Mota <klebermo@gmail.com>
2016-04-22 20:53:55 -0300

    UsuarioDao.java

---
+++ src/main/java/org/kleber/app/model/usuario/UsuarioDao.java
@@ -0,0 +1,9 @@
+package org.kleber.app.model.usuario;
+
+import org.kleber.app.generic.Dao;
+
+public class UsuarioDao extends Dao<Usuario> {
+  public UsuarioDao() {
+    super(Usuario.class);
+  }
+}
