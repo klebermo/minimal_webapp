748768840ba447be7fdb58e35dfc8b9d7b3eee85
Kleber Mota <klebermo@gmail.com>
2016-04-22 20:21:20 -0300

    LoginServlet

--- src/main/java/org/kleber/app/servlet/LoginServlet.java
+++ src/main/java/org/kleber/app/servlet/LoginServlet.java
@@ -4,10 +4,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
 
 public class LoginServlet extends HttpServlet {
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException {
-    //
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    String nextJSP = "/signin.jsp";
+    RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
+    dispatcher.forward(request,response);
   }
 
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
