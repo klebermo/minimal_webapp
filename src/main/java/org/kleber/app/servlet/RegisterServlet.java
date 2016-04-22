c817ed2a02260d9cb3783fea06169d7ab491751b
Kleber Mota <klebermo@gmail.com>
2016-04-22 20:54:32 -0300

    RegisterServlet.java

--- src/main/java/org/kleber/app/servlet/RegisterServlet.java
+++ src/main/java/org/kleber/app/servlet/RegisterServlet.java
@@ -4,13 +4,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
 
 public class RegisterServlet extends HttpServlet {
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException {
-    //
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    String nextJSP = "/WEB-INF/jsp/signup.jsp";
+    RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
+    dispatcher.forward(request,response);
   }
 
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     //
   }
 }
